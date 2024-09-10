43b78f356333562881e6e8e965baaf66a9e7b1be
Quan <qhoang2373@gmail.com>
2024-09-09 18:12:06 -0700

    create skeleton server

---
+++ server.js
@@ -0,0 +1,21 @@
+// Here is where we import modules
+// We begin by loading Express
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+// GET /
+app.get("/", async (req, res) => {
+    res.send("hello, friend!");
+  });
+
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+  
