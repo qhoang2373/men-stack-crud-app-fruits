6df9514da0170a8e5f38dbd0d4eae0f445a9db96
Quan <qhoang2373@gmail.com>
2024-09-09 18:59:24 -0700

    add fruit model and new fruit form

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 // Here is where we import modules
+
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+
 // We begin by loading Express
 const express = require("express");
+const mongoose = require("mongoose"); // require package
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
-
-
-// GET /
-app.get("/", async (req, res) => {
-    res.send("hello, friend!");
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
   });
 
+const Fruit = require("./models/fruit.js");
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
+  });
+
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.ejs");
+  });
+  
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
   });
   
